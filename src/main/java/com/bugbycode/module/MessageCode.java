572727c592026baad6b73c5c2c410b34c1459d83
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-11-15 17:47:20 +0800

    首次创建

--- src/main/java/com/bugbycode/module/MessageCode.java
+++ src/main/java/com/bugbycode/module/MessageCode.java
@@ -4,7 +4,7 @@ public abstract class MessageCode {
 	
 	public final static int HEARTBEAT = 0; 	//心跳通信
 	
-	public final static int REGISTER = 1;	//注册
+	public final static int AUTH = 1;	//认证
 	
 	public final static int CONNECTION = 2;	//连接目标设备
 	
@@ -15,4 +15,8 @@ public abstract class MessageCode {
 	public final static int TRANSFER_DATA = 3; //流量转发
 	
 	public final static int CLOSE_CONNECTION = 4; //断开与目标设备的连接
+	
+	public final static int AUTH_ERROR = 5; //认证失败
+	
+	public final static int AUTH_SUCCESS = 6; //认证成功
 }
