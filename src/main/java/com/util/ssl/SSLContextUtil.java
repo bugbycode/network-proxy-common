ba025c62edabc95cfbc5df0d1a9fdd88e92c37e0
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-11-19 18:32:32 +0800

    首次创建

--- src/main/java/com/util/ssl/SSLContextUtil.java
+++ src/main/java/com/util/ssl/SSLContextUtil.java
@@ -11,13 +11,13 @@ import java.security.cert.CertificateException;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLException;
-
+import javax.net.ssl.TrustManagerFactory;
 
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 
 public class SSLContextUtil {
-	public static SslContext getContext(String keystorePath,String password) {
+	public static SslContext getServerContext(String keystorePath,String password) {
 		SslContext sslContext;
 		KeyManagerFactory keyManagerFactory = null;
 		try {
@@ -45,6 +45,34 @@ public class SSLContextUtil {
 			e.printStackTrace();
 			throw new RuntimeException(e.getMessage());
 		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		}
+		return sslContext;
+	}
+	
+	public static SslContext getClientContext(String keystorePath,String password) {
+		TrustManagerFactory tf = null; 
+		SslContext sslContext = null;
+		try {
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			keyStore.load(new FileInputStream(keystorePath), password.toCharArray());
+			tf = TrustManagerFactory.getInstance("SunX509");
+            tf.init(keyStore);
+            sslContext = SslContextBuilder.forClient().trustManager(tf).build();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} catch (CertificateException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} catch (KeyStoreException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e.getMessage());
 		}
