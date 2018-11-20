2b6733268c8fcf20292c24bdceedf8b2a9c3bc42
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-11-20 12:54:38 +0800

    首次创建

--- src/main/java/com/bugbycode/handler/MessageDecoder.java
+++ src/main/java/com/bugbycode/handler/MessageDecoder.java
@@ -27,62 +27,67 @@ public class MessageDecoder extends LengthFieldBasedFrameDecoder {
 
 	@Override
 	protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
-		
-		in = (ByteBuf) super.decode(ctx,in);  
-		
-		if(in == null){
-            return null;
-        }
-		
-		if(in.readableBytes() < HEADER_SIZE){
-            throw new Exception("头部信息长度错误");
-        }
-		//读取token信息 总共32个字节
-		byte[] token_buff = new byte[32];
-		in.readBytes(token_buff);
-		//读取消息类型总共4字节
-		int type = in.readInt();
-		//读取长度 总共4个字节
-		int length = in.readInt();
-		
-		if(in.readableBytes() != length) {
-			 throw new Exception("标记的长度不符合实际长度");
-		}
-		byte[] data = {};
-		if(length > 0) {
-			data = new byte[length];
-			in.readBytes(data);
-		}
-		
+
 		Message message = new Message();
 		
-		if(!(type == MessageCode.HEARTBEAT || type == MessageCode.AUTH ||
-				type == MessageCode.AUTH_ERROR || type == MessageCode.AUTH_SUCCESS)) {
-			String token = new String(token_buff);
-			message.setToken(token);
-		}
-		
-		message.setType(type);
-		
-		//以下是消息内容
-		if(type == MessageCode.AUTH) {
-			String authInfo = new String(data);
-			JSONObject json = new JSONObject(authInfo);
-			Authentication auth = new Authentication(json.getString("username"), json.getString("password"));
-			message.setData(auth);
-		}else if(type == MessageCode.CONNECTION) {
-			String connInfo = new String(data);
+		try {
+			in = (ByteBuf) super.decode(ctx,in);  
 			
-			JSONObject json = new JSONObject(connInfo);
+			if(in == null){
+	            return null;
+	        }
 			
-			ConnectionInfo conn = new ConnectionInfo(json.getString("host"), json.getInt("port"));
-			message.setData(conn);
-		}else if(type == MessageCode.TRANSFER_DATA) {
-			message.setData(data);
+			if(in.readableBytes() < HEADER_SIZE){
+	            throw new Exception("头部信息长度错误");
+	        }
+			//读取token信息 总共32个字节
+			byte[] token_buff = new byte[32];
+			in.readBytes(token_buff);
+			//读取消息类型总共4字节
+			int type = in.readInt();
+			//读取长度 总共4个字节
+			int length = in.readInt();
+			
+			if(in.readableBytes() != length) {
+				 throw new Exception("标记的长度不符合实际长度");
+			}
+			byte[] data = {};
+			if(length > 0) {
+				data = new byte[length];
+				in.readBytes(data);
+			}
+			
+			if(!(type == MessageCode.HEARTBEAT || type == MessageCode.AUTH ||
+					type == MessageCode.AUTH_ERROR || type == MessageCode.AUTH_SUCCESS)) {
+				String token = new String(token_buff);
+				message.setToken(token);
+			}
+			
+			message.setType(type);
+			
+			//以下是消息内容
+			if(type == MessageCode.AUTH) {
+				String authInfo = new String(data);
+				JSONObject json = new JSONObject(authInfo);
+				Authentication auth = new Authentication(json.getString("username"), json.getString("password"));
+				message.setData(auth);
+			}else if(type == MessageCode.CONNECTION) {
+				String connInfo = new String(data);
+				
+				JSONObject json = new JSONObject(connInfo);
+				
+				ConnectionInfo conn = new ConnectionInfo(json.getString("host"), json.getInt("port"));
+				message.setData(conn);
+			}else if(type == MessageCode.TRANSFER_DATA) {
+				message.setData(data);
+			}
+			return message;
+		}catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			in.release();
 		}
-		
-		in.release();
-		return message;
 	}
 
 	
